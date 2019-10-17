c3a6833ee6e4447e94b672930001ced2f5ab5782
zatuz <39281593+zatuz@users.noreply.github.com>
2019-10-17 02:41:23 -0500

    Create pre-pre.sh

---
+++ pre-pre.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o pipefail
+echo 'LANG="es_MX.UTF-8"' > /etc/default/locale
+echo 'LANGUAGE="es_MX.UTF-8"' >> /etc/default/locale
+echo 'LC_ALL="es_MX.UTF-8"' >> /etc/default/locale
+echo "es_MX.UTF-8 UTF-8" > /etc/locale.gen
+apt update
+locale-gen
+apt upgrade -y
+apt dist-upgrade -y
+apt install -y git curl wget nano
+reboot
