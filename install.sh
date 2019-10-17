4aafde392f6ce511db76a7138b746eb3ffb6f9c5
zatuz <39281593+zatuz@users.noreply.github.com>
2019-10-17 02:54:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ mkdir "$HOME/complementos"
 cd "$HOME/complementos" || exit
 wget "https://git.io/Je4gs" -O .env
 wget "https://git.io/Je4gc" -O pre.sh # https://git.io/Je4go
-wget "https://git.io/Je4gw" -O post.sh
-wget "https://git.io/Je4gK" -O .zshrc-1 
-wget "https://git.io/Je4gF" -O pre-pre.sh
+wget "https://raw.githubusercontent.com/zatuz/pre-vagrant/master/post.sh?token=AJLWHOJQAW5VSY7XBQPF3WS5VAPGK" -O post.sh
+wget "https://raw.githubusercontent.com/zatuz/pre-vagrant/master/.zshrc-1?token=AJLWHOLO2GV7WEP4CIY4NZC5VAPEU" -O .zshrc-1 
+wget "https://raw.githubusercontent.com/zatuz/pre-vagrant/master/pre-pre.sh?token=AJLWHOPIS33NO2QPO7BR3NK5VAPAM" -O pre-pre.sh
 
 # damos permisos de ejecutable sh
 chmod +x *.sh
