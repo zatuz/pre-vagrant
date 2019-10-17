8a9a2d026ea22862b88a633c2b9de9dc135364a3
zatuz <39281593+zatuz@users.noreply.github.com>
2019-10-17 02:52:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ wget "https://git.io/Je4gs" -O .env
 wget "https://git.io/Je4gc" -O pre.sh # https://git.io/Je4go
 wget "https://git.io/Je4gw" -O post.sh
 wget "https://git.io/Je4gK" -O .zshrc-1 
-wget "https://git.io/Je4gy" -O pre-pre.sh
+wget "https://git.io/Je4gF" -O pre-pre.sh
 
 # damos permisos de ejecutable sh
 chmod +x *.sh
