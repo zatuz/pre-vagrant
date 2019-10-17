e697af7ffaa5bd2ee2caa6a4b58cfab541119f98
zatuz <39281593+zatuz@users.noreply.github.com>
2019-10-17 02:46:12 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -o pipefail
+## creamos carpeta complementos
+mkdir "$HOME/complementos"
+cd "$HOME/complementos" || exit
+wget "https://git.io/Je4gs" -O .env
+wget "https://git.io/Je4gc" -O pre.sh # https://git.io/Je4go
+wget "https://git.io/Je4gw" -O post.sh
+wget "https://git.io/Je4gK" -O .zshrc-1 
+wget "https://git.io/Je4gy" -O pre-pre.sh
+
+# damos permisos de ejecutable sh
+chmod +x *.sh
+#iniciamos pre.sh 
+source "pre.sh"
+sudo ./pre-pre.sh
