fd79df83d2e598d520e2a85ce1a9b4ee2f5777bd
zatuz <39281593+zatuz@users.noreply.github.com>
2019-10-25 17:49:21 -0500

    rep

--- post.sh
+++ post.sh
@@ -9,14 +9,14 @@ else
     exit 1
 fi
 if [ ! -d "$HOME"/.oh-my-zsh ]; then
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 fi
 #instalamos un plugin
 if [ ! -d "$ZSHPLUGIN"zsh-syntax-highlighting ]; then
-$CLONE https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+    $CLONE https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 if [ ! -d "$ZSHPLUGIN"zsh-autosuggestions ]; then
-$CLONE git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+    $CLONE https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 fi
 #copiamos la configuracion de zsh
 cp  $ZSH1 "$HOME"/.zshrc
